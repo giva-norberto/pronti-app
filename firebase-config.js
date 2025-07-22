2c5876c84e323637f597461f3f69de45c1a7f51f
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-07-22 20:46:06 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,18 @@
-// Usamos a URL completa do Firebase para garantir que o navegador encontre o código
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+// firebase-config.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCnGK3j90_UpBdRpu5nhSs-nY84I_e0cAk",
   authDomain: "pronti-app-37c6e.firebaseapp.com",
   projectId: "pronti-app-37c6e",
-  storageBucket: "pronti-app-37c6e.appspot.com", // Corrigi para o padrão .appspot.com, que é mais comum.
+  storageBucket: "pronti-app-37c6e.appspot.com",
   messagingSenderId: "736700619274",
   appId: "1:736700619274:web:557aa247905e56fa7e5df3"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-export { app };
+// Inicializa o Firestore e exporta para usar em outros arquivos
+export const db = getFirestore(app);
