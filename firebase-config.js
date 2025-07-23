3394a13f3ef1d80abc71f17648cc85c668f1ee8d
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-07-22 21:54:59 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCnGK3j90_UpBdRpu5nhSs-nY84I_e0cAk",
   authDomain: "pronti-app-37c6e.firebaseapp.com",
   projectId: "pronti-app-37c6e",
-  storageBucket: "pronti-app-37c6e.appspot.com",  // Atenção aqui: corrigir para ".appspot.com"
+  storageBucket: "pronti-app-37c6e.appspot.com",
   messagingSenderId: "736700619274",
   appId: "1:736700619274:web:557aa247905e56fa7e5df3"
 };
 
-// Inicializa app Firebase
 const app = initializeApp(firebaseConfig);
-
-// Instância dos serviços que vai usar
 const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { app, db, auth };
+
