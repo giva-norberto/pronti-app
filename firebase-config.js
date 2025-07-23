6c2211f2f2860435dd5ed4e591b5721c3ad92b59
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-07-22 21:39:27 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,21 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnGK3j90_UpBdRpu5nhSs-nY84I_e0cAk",
-  authDomain: "pronti-app-37c6e.firebaseapp.com",
-  projectId: "pronti-app-37c6e",
-  storageBucket: "pronti-app-37c6e.appspot.com",
-  messagingSenderId: "736700619274",
-  appId: "1:736700619274:web:557aa247905e56fa7e5df3"
+  apiKey: "AIzaSyCbtpWQ7r4qlIktOOP93nQQxUyO9ReP1NA",
+  authDomain: "pronti-app.firebaseapp.com",
+  projectId: "pronti-app",
+  storageBucket: "pronti-app.appspot.com",
+  messagingSenderId: "18706493173",
+  appId: "1:18706493173:web:b7798e2e6c2a18e65ef689"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { app, db, auth, storage };
