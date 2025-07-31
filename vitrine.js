4daf98e20ad69708b6634bf6ddc9e3f4400f89b0
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-07-31 17:22:46 -0300

    Update vitrine.js

--- vitrine.js
+++ vitrine.js
@@ -1,8 +1,8 @@
-// vitrine.js (arquivo de entrada)
+// vitrine.js (arquivo principal de entrada)
 
-import './vitrini-firebase.js';       // Inicializa Firebase
-import './vitrini-auth.js';           // Login, logout, autenticação
-import './vitrini-data.js';           // Carrega dados do Firestore
-import './vitrini-agendamento.js';    // Lógica de agendamento
-import './vitrini-ui.js';             // Atualiza elementos da interface
-import './vitrini-events.js';         // Escuta cliques, inputs, abas, etc
+import './vitrini-firebase.js';
+import './vitrini-auth.js';
+import './vitrini-agendamento.js';
+import './vitrini-ui.js';
+import './vitrini-utils.js';
+
