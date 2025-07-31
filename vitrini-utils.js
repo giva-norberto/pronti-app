3701e3088569f2194098d242b2eb21444851532a
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-07-31 17:05:42 -0300

    Create vitrini-utils.js

---
+++ vitrini-utils.js
@@ -0,0 +1,11 @@
+// vitrini-utils.js
+export function showNotification(message, erro = false) {
+    const container = document.getElementById('notificacao');
+    if (!container) return;
+    container.textContent = message;
+    container.style.color = erro ? 'red' : 'green';
+    container.style.opacity = '1';
+    setTimeout(() => {
+        container.style.opacity = '0';
+    }, 3000);
+}
