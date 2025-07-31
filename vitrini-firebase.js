7db14fa71eec23399dc809461a31fa08cfd7250e
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-07-31 17:02:55 -0300

    Create vitrini-firebase.js

---
+++ vitrini-firebase.js
@@ -0,0 +1,20 @@
+// vitrini-firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCnGK3j90_UpBdRpu5nhSs-nY84I_e0cAk",
+    authDomain: "pronti-app-37c6e.firebaseapp.com",
+    projectId: "pronti-app-37c6e",
+    storageBucket: "pronti-app-37c6e.appspot.com",
+    messagingSenderId: "736700619274",
+    appId: "1:736700619274:web:557aa247905e5df3"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { app, db, auth, provider };
