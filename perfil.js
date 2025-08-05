6700f5b1803a46aa4395839a070398b6d46ce6bc
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-08-04 23:20:59 -0300

    Update perfil.js

--- perfil.js
+++ perfil.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCnGK3j90_UpBdRpu5nhSs-nY84I_e0cAk",
   authDomain: "pronti-app-37c6e.firebaseapp.com",
   projectId: "pronti-app-37c6e",
-  storageBucket: "pronti-app-37c6e.appspot.com",
+  storageBucket: "pronti-app-37c6e.firebasestorage.app", // <--- LINHA CORRIGIDA
   messagingSenderId: "736700619274",
   appId: "1:736700619274:web:557aa247905e56fa7e5df3"
 };
