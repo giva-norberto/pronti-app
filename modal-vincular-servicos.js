2ded5d551454de7eb17a012d98061b0173896f4e
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-08-06 14:09:19 -0300

    Create modal-vincular-servicos.js

---
+++ modal-vincular-servicos.js
@@ -0,0 +1,14 @@
+<!-- Modal para vincular serviços ao funcionário -->
+<div id="modal-servicos-funcionario" class="modal-bg" style="display: none;">
+  <div class="modal-card" style="max-width: 420px;">
+    <h2>Serviços do Funcionário</h2>
+    <p>Selecione os serviços que este funcionário realiza:</p>
+    <form id="form-servicos-funcionario">
+      <div id="lista-servicos-checkbox"></div>
+      <div style="display:flex;gap:10px; margin-top:18px;">
+        <button type="button" class="btn-confirmar" id="btn-salvar-servicos">Salvar</button>
+        <button type="button" class="btn-secondary" id="btn-cancelar-servicos">Cancelar</button>
+      </div>
+    </form>
+  </div>
+</div>
