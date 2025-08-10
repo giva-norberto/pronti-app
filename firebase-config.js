23015db1a67bea0dbc4f9ccbbb35cab39e9ac5a9
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-08-10 15:55:49 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,6 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// Configuração do seu projeto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBOfsPIr0VLCuZsIzOFPsdm6kdhLb1VvP8",
   authDomain: "pronti-app-37c6e.firebaseapp.com",
@@ -13,11 +12,13 @@ const firebaseConfig = {
   appId: "1:736700619274:web:557aa247905e56fa7e5df3"
 };
 
-// Inicializa o Firebase App apenas uma vez!
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporte TUDO que vai usar nos outros arquivos!
-export { app };
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
+// Inicializa os serviços
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+// Exporta tudo
+export { app, db, auth, storage };
