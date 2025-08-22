69888fc5346aee62d02cc289583b351b84558a39
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-08-22 09:59:44 -0300

    Update vitrini-auth.js

--- vitrini-auth.js
+++ vitrini-auth.js
@@ -24,7 +24,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import { showAlert } from './vitrini-utils.js';
-// REMOVIDO: A importação que causava o erro foi removida.
 
 // ======================================================================
 //   FUNÇÃO DO MODAL (agora dentro deste arquivo para evitar erros)
