bcafc4ad73d10991225c1ed38ba5137c8c0dc1b4
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-08-27 19:15:11 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,8 @@
-// Firebase config para pronti-app (projeto antigo)
-
 import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// ATENÇÃO: storageBucket corrigido para .appspot.com
 const firebaseConfig = {
   apiKey: "AIzaSyA1CL5SbSWXe9843dgiopnmahCsrsF--us",
   authDomain: "pronti-app-37c6e.firebaseapp.com",
@@ -15,20 +12,12 @@ const firebaseConfig = {
   appId: "1:736700619274:web:557aa247905e56fa7e5df3"
 };
 
-// Inicializa o Firebase APENAS UMA VEZ
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
-// Instâncias únicas
 export { app };
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+export const provider = new GoogleAuthProvider(); // só uma vez!
 export const storage = getStorage(app);
 
-// Persistência local para Auth (opcional, mas recomendado)
-setPersistence(auth, browserLocalPersistence).catch(() => {});
-export const provider = new GoogleAuthProvider();
-export const storage = getStorage(app);
-
-// Garante que a persistência de login será local (mesmo após fechar o navegador)
 setPersistence(auth, browserLocalPersistence).catch(() => {});
