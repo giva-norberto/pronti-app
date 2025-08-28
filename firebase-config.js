37233bb51755b0aa2104257bd31ea434a409f0c6
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-08-28 10:34:25 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,16 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
 
-// SUA CONFIGURAÇÃO DO FIREBASE
+// Sua configuração do Firebase para pronti-app-37c6e
 const firebaseConfig = {
-  apiKey: "AIzaSyBIT61ii28vbYyi5oNRDRy8vNx3U4XDVfo",
-  authDomain: "pronti-novo.firebaseapp.com",
-  projectId: "pronti-novo",
-  storageBucket: "pronti-novo.appspot.com", // CORRIGIDO: .app -> .app**spot**.com
-  messagingSenderId: "315046501183",
-  appId: "1:315046501183:web:2f188bfd00b448aa64518a"
+  apiKey: "AIzaSyA1CL5SbSWXe9843dgiopnmahCsrsF--us",
+  authDomain: "pronti-app-37c6e.firebaseapp.com",
+  projectId: "pronti-app-37c6e",
+  storageBucket: "pronti-app-37c6e.appspot.com", // CORRIGIDO: .firebasestorage.app -> .appspot.com
+  messagingSenderId: "736700619274",
+  appId: "1:736700619274:web:557aa247905e56fa7e5df3"
 };
 
 // Inicializa o Firebase e exporta instâncias SEMPRE DA MESMA VERSÃO!
@@ -19,3 +18,4 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+export const provider = new GoogleAuthProvider();
