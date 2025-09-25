23479c55e4a23dc29e9ae77cdfc1b2fb2257fe59
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-09-25 10:50:47 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,16 +1,16 @@
 // ======================================================================
-//           ARQUIVO firebase-messaging-sw.js (VERSÃO CORRETA E SEGURA)
+// 	 	 	 ARQUIVO firebase-messaging-sw.js (VERSÃO CORRETA E SEGURA)
 // ======================================================================
 
 // Este código usa 'importScripts' que é a forma correta de carregar
 // as bibliotecas do Firebase dentro de um Service Worker.
 
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js' );
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js' );
 
 // Suas credenciais do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBOfsPIr0VLCuZsIzOFPsdm6kdhLb1VvP8",
+  apiKey: "AIzaSyC9aewvqMIfAcXeKH0Y8KMn3-clYL4GTv0",
   authDomain: "pronti-app-37c6e.firebaseapp.com",
   projectId: "pronti-app-37c6e",
   storageBucket: "pronti-app-37c6e.appspot.com",
@@ -25,3 +25,4 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // O arquivo agora está pronto para receber notificações em segundo plano.
+ra receber notificações em segundo plano.
